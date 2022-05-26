b802af0712224f55be03d961c557c33fe966d0a6
tienyiah <tienyiah@uci.edu>
2022-05-25 23:35:14 -0700

    Add in the coordinate

--- src/plot_ZATOM_bifurcation_typeB_for_phase.sh
+++ src/plot_ZATOM_bifurcation_typeB_for_phase.sh
@@ -3,17 +3,17 @@
 res=5
 
 python3 src/plot_scans_and_snapshots.py --folder \
-   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi-0000100_pos/lb$res  \
-   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi-0000075_pos/lb$res  \
-   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi-0000050_pos/lb$res  \
-   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi-0000025_pos/lb$res  \
-   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000000_pos/lb$res  \
-   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000025_pos/lb$res  \
-   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000050_pos/lb$res  \
    ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000075_pos/lb$res  \
+   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000080_pos/lb$res  \
+   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000085_pos/lb$res  \
+   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000090_pos/lb$res  \
+   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000095_pos/lb$res  \
    ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000100_pos/lb$res  \
+   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000105_pos/lb$res  \
+   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000110_pos/lb$res  \
+   ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000115_pos/lb$res  \
    ./data/continuation_data_0522/batch_std20/output_redo_balanced_tanh/CM_xi00000200_pos/lb$res  \
-    --legend '$\xi=-1$' '$\xi=-.75$' '$\xi=-.5$' '$\xi=-.25$' '$\xi=0$' '$\xi=.25$' '$\xi=.5$' '$\xi=.75$' '$\xi=1$' '$\xi=2$' \
+    --legend '$\xi=.75$' '$\xi=.80$' '$\xi=.85$' '$\xi=.90$' '$\xi=.95$' '$\xi=1$' '$\xi=1.05$' '$\xi=1.10$' '$\xi=1.15$' '$\xi=2$'\
     --x-var "gamma" --y-var "psi" \
     --x-rng -0.01 0.6 --y-rng 0 12 --metric fixed --metric-dep 1000.0 --metric-lat 50 \
     --residue-threshold 3e-10
