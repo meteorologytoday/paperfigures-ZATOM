b75c1c9eb6dc30fed589104c31875f35fb3272f4
t2hsu <t2hsu@aha.ucsd.edu>
2024-11-03 16:46:37 -0800

    Need to work on expanse

--- src/make_extended_data.py
+++ src/make_extended_data.py
@@ -8,6 +8,18 @@ alpha_S = 7e-1   # kg/m^3/PSU
 g0 = 10.0        # m/s^2
 rho0 = 1e3       # kg/m^3
 beta = 0.9
+print("Beta = ", beta)
+
+
+T2b_factor = alpha_T * g0 / rho0
+S2b_factor = - alpha_S * g0 / rho0
+
+def TS2b(T, S):
+    return T * T2b_factor + S * S2b_factor
+
+# Variables dimension assumes to be:
+# (Sample, X, Y, Z)
+
 
 """
     This function load the scan data.
@@ -17,7 +29,7 @@ beta = 0.9
     It also load the coordinate data. It selects the first
     xxxx.nc file.
 """
-def makeExtendedData(data, coor):
+def makeExtendedData(data, coor, lat_s = 40.0, lat_n = 68.0):
     
     Ns = data["Psib"].shape[0]
 
@@ -25,10 +37,7 @@ def makeExtendedData(data, coor):
     psi_W = (data["Psib"][:, :-1, :] + data["Psib"][:, 1:, :]) / 2
 
     # Next few line is to remove integration of certain latitudes
-    lat_s = 40.0
     lat_s_ind = np.argmin(np.abs(coor["y_T"] - lat_s))
-
-    lat_n = 68.0
     lat_n_ind = np.argmin(np.abs(coor["y_T"] - lat_n))
 
     print("Integrate only the range between lat [%f, %f], with idx founc [%d, %d]" % (
@@ -50,7 +59,7 @@ def makeExtendedData(data, coor):
     mode_1_dz_weight_W = - (2 / H) * np.sin( coor["z_W"] * np.pi / H) * coor["dz_W"]
     
     print("Total depth H = ", H)
-    print("Beta = ", beta)
+
     #b_mean = ( data["bw"] * Lw + data["be"] * Le ) / ( Lw + Le )
     b_eff  = (1 - beta) * data["bw"] + data["be"] * Lambda
     s_eff_W = ( b_eff[:, :, :-1] - b_eff[:, :, 1:] ) / ( coor["dz_W"][1:-1][None, None, :] )
@@ -154,5 +163,240 @@ def computeConvectivity(flags):
                 #break  # only count if it is unstable all the way to the top
     
     return cnt / flags.size
+
+
+
+
+def mode_integration(d, wgt, axis=-1):
+
+    if axis == -1:
+        axis = len(d.shape)-1
+
+    return np.sum(d * wgt, axis=axis)
+
+def yavg(d, wgt, axis=-1):
+
+    if axis == -1:
+        axis = len(d.shape)-1
+
+    return np.average(d, weights=wgt, axis=axis)
+
+# This function computes the exact terms for diagnostic relationship
+def makeExtendedData2(data, coor, lat_s = 40.0, lat_n = 68.0, verbose = False):
+   
+    new_data = {}
+ 
+    Ns = data["Psib"].shape[0]
+    
+    # Compute bw^*
+    new_data["bw_bnd"] = data["be"] + 2 * ( data["bw"] - data["be"] )
+    psi_W = (data["Psib"][:, :-1, :] + data["Psib"][:, 1:, :]) / 2
+
+    # Remove integration of certain latitudes
+    lat_s_ind = np.argmin(np.abs(coor["y_T"] - lat_s))
+    lat_n_ind = np.argmin(np.abs(coor["y_T"] - lat_n))
+
+    verbose and print("Integrate only the range between lat [%f, %f], with idx founc [%d, %d]" % (
+        lat_s, lat_n,
+        lat_s_ind, lat_n_ind,
+    ))
+    
+    coor["cos_lat"][0:lat_s_ind] = 0.0
+    coor["cos_lat"][lat_n_ind:] = 0.0
+    verbose and print( "Cleared up cos_lat weighting. Result: coor['cos_lat'] = ", coor["cos_lat"])
+
+    # Compute geometric factor
+    Lw = coor["dx_T"][0, 0]
+    Le = coor["dx_T"][1, 0]
+    Lambda = Lw / Le
+
+    H = np.sum(coor["dz_T"])
+    verbose and print("Total depth H = ", H)
+    
+    mode_1_dz_weight_T = - (2 / H) * np.sin( coor["z_T"] * np.pi / H) * coor["dz_T"]
+    mode_1_dz_weight_W = - (2 / H) * np.sin( coor["z_W"] * np.pi / H) * coor["dz_W"]
+   
+    
+
+
+ 
+    # Here, `s` stands for stability, defined as s=db/dz
+    
+    # These are old code to derive effective stability, considering the cancellation betwen
+    # MOC and ZOC advection.
+
+    #b_eff  = (1 - beta) * data["bw"] + data["be"] * Lambda
+    #s_eff_W = ( b_eff[:, :, :-1] - b_eff[:, :, 1:] ) / ( coor["dz_W"][1:-1][None, None, :] )
+    #s_eff_vint = np.sum(s_eff_W * mode_1_dz_weight_W[None, None, 1:-1], axis=2)
+    #data["mode1_s_eff"] = np.average( s_eff_vint, weights=coor["cos_lat"], axis=1)
+    
+    # Here I want to compute the exact terms
+    
+    # ZOC component
+    w_ZOC = data["we"] 
+    w_MOC = data["ww"] + data["we"]
+    
+    dbedz_W = ( data["be"][:, :, :-1] - data["be"][:, :, 1:] ) / ( coor["dz_W"][1:-1][None, None, :] )
+    dbwdz_W = ( data["bw"][:, :, :-1] - data["bw"][:, :, 1:] ) / ( coor["dz_W"][1:-1][None, None, :] )
+    dbwdy_V = ( data["bw"][:, 1:, :] - data["bw"][:, :-1, :] ) / ( coor["dy_V"][1:-1][None, None, :] )
+    
+    eff_dbdz_W = Lambda * dbedz_W + dbwdz_W
+    new_data["mode1_dbdiffdt_dueto_ZOC"] = yavg(
+        mode_integration(
+            w_ZOC[:, :, 1:-1] * eff_dbdz_W,
+            wgt=mode_1_dz_weight_W[1:-1],
+        ),
+        wgt=coor["cos_lat"],
+    )
+
+    # MOC component
+    MOC_vert_adv = yavg(
+        mode_integration(
+            w_MOC[:, :, 1:-1] * dbwdz_W,
+            wgt=mode_1_dz_weight_W[1:-1],
+        ),
+        wgt=coor["cos_lat"],
+    )
+
+    MOC_horz_adv = yavg(
+        mode_integration(
+            data["vw"][:, 1:-1, :] * dbwdy_V,
+            wgt=mode_1_dz_weight_W[1:-1],
+        ),
+        wgt=coor["cos_lat"],
+    )
+
+
+
+
+    
+
+ 
+    # `SS` stands for "source and sink"
+    SS_T = data["X_SS_"][:, 0, :, :, :]
+    SS_S = data["X_SS_"][:, 1, :, :, :]
+    
+    SS_b_dueto_T = SS_T * T2b_factor
+    SS_b_dueto_S = SS_S * S2b_factor
+    
+    dbdiffdt_dueto_SS_T = SS_b_dueto_T[:, 1, :, :] - SS_b_dueto_T[:, 0, :, :]
+    dbdiffdt_dueto_SS_S = SS_b_dueto_S[:, 1, :, :] - SS_b_dueto_S[:, 0, :, :]
+ 
+    new_data["mode1_dbdiffdt_dueto_SS_T"] = yavg(
+            mode_integration(
+                dbdiffdt_dueto_SS_T,
+                wgt=mode_1_dz_weight_T,
+            ),
+            wgt=coor["cos_lat"],
+    )
+
+    new_data["mode1_dbdiffdt_dueto_SS_S"] = yavg(
+            mode_integration(
+                dbdiffdt_dueto_SS_S,
+                wgt=mode_1_dz_weight_T,
+            ),
+            wgt=coor["cos_lat"],
+    )
+   
+    # Diffusion
+    VDIFU_T = data["X_VDIFU_"][:, 0, :, :, :]
+    VDIFU_S = data["X_VDIFU_"][:, 1, :, :, :]
+    HDIFU_T = data["X_HDIFU_"][:, 0, :, :, :]
+    HDIFU_S = data["X_HDIFU_"][:, 1, :, :, :]
+
+    VDIFU_b = TS2b(VDIFU_T, VDIFU_S)
+    HDIFU_b = TS2b(HDIFU_T, HDIFU_S)
+
+    dbdiffdt_dueto_VDIFU = Lambda * VDIFU_b[:, 1, :, :] - VDIFU_b[:, 0, :, :]
+    dbdiffdt_dueto_HDIFU = Lambda * HDIFU_b[:, 1, :, :] - HDIFU_b[:, 0, :, :]
+
+    new_data["mode1_dbdiffdt_dueto_VDIFU"] = yavg(
+            mode_integration(
+                dbdiffdt_dueto_VDIFU,
+                wgt=mode_1_dz_weight_T,
+            ),
+            wgt=coor["cos_lat"],
+    )
+ 
+    new_data["mode1_dbdiffdt_dueto_HDIFU"] = yavg(
+            mode_integration(
+                dbdiffdt_dueto_HDIFU,
+                wgt=mode_1_dz_weight_T,
+            ),
+            wgt=coor["cos_lat"],
+    )
+ 
+    # ui component
+    dbdiffdt_dueto_ui = data["ui"] * (data["be"] - data["bw"]) / Lw
+
+ 
+    chi_vint = np.sum(data["chi"] * mode_1_dz_weight_W[None, None, :], axis=2)
+    data["mode1_chi"] = np.average( chi_vint, weights=coor["cos_lat"], axis=1)
+    
+    data["mode1_chi_dbdz_product"] = data["mode1_chi"] * data["mode1_s_eff"] / Lw 
+
+    chi_dbdz = s_eff_W * data["chi"][:, :, 1:-1] / Lw
+    chi_dbdz_vint = np.sum( chi_dbdz * mode_1_dz_weight_W[None, None, 1:-1], axis=2)
+    data["mode1_chi_dbdz"] = np.average( chi_dbdz_vint[:, :], weights=coor["cos_lat"], axis=1)
+
+
+    f_co = 2 * omega * coor["sin_lat"]
+    
+    f_W = (psi_W[0, :, :] * 0 + 1.0) * f_co[:, None]  # borrow the shape from psi_W
+    mode1_f = np.average( np.sum( f_W * mode_1_dz_weight_W[None, :], axis=1), weights=coor["cos_lat"], axis=0 )
+    print("mode1_f = ", mode1_f)
+
+    fpsi_W = f_co[None, :, None] * psi_W
+    fpsi_vint = np.sum( fpsi_W * mode_1_dz_weight_W[None, None, :], axis=2)
+    data["mode1_psi"] = np.average( fpsi_vint, weights=coor["cos_lat"], axis=1) / mode1_f
+    
+    db_ew = data["be"] - data["bw_bnd"]
+    db_ew = np.sum(db_ew * mode_1_dz_weight_T[None, None, :], axis=2)
+    db_ew = np.average(db_ew, weights=coor["cos_lat"], axis=1) 
+    data["mode1_db_ew"] = db_ew
+
+    # Convective mixing and convectivity
+  
+    be = data["be"]
+    bw = data["bw"] 
+    dwf_east = np.zeros(be.shape)
+    dwf_west = np.zeros(be.shape)
+
+    cvt_e = np.zeros( (Ns,) )
+    cvt_w = np.zeros( (Ns,) )
+
+    for j in range(Ns):
+
+        flags_e = computeDeepWaterFormationGrids(be[j, :, :])
+        flags_w = computeDeepWaterFormationGrids(bw[j, :, :])
+
+        dwf_east[j, :, :] = flags_e
+        dwf_west[j, :, :] = flags_w
+
+        cvt_e[j] = computeConvectivity(flags_e) 
+        cvt_w[j] = computeConvectivity(flags_w) 
+
+    data["dwf_east"] = dwf_east
+    data["dwf_west"] = dwf_west
+    data["cvt_e"] = cvt_e
+    data["cvt_w"] = cvt_w
+    data["d_cvt"] = cvt_w - cvt_e
+
+
+    if ("qe" in data) and ("qw" in data):
+        print("Compute dq")
+        
+        # notice that q is separated into T and S components. So z is now the fourth axis
+        qw_vint = np.sum( data["qw"][:, :, :, :] * mode_1_dz_weight_T[None, None , None, :], axis=3)
+        qw = np.average(qw_vint, weights=coor["cos_lat"], axis=2) 
+ 
+        qe_vint = np.sum( data["qe"][:, :, :, :] * mode_1_dz_weight_T[None, None , None, :], axis=3)
+        qe = np.average(qe_vint, weights=coor["cos_lat"], axis=2) 
+        
+        # Convert T and S into buoyancy
+        qw_b = g0 / rho0 * (qw[:, 0] * alpha_T - qw[:, 1] * alpha_S) 
+        qe_b = g0 / rho0 * (qe[:, 0] * alpha_T - qe[:, 1] * alpha_S) 
+
+        data["mode1_dq"] = qe_b - qw_b
 
 
